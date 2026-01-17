bc608700785dba37e49ba64646a235d985af0a21
0x-DevSec <h.annane.dev@protonmail.com>
2026-01-17 18:38:11 +0100

    connect to database using Singleton

---
+++ app/helpers/Database.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Helpers;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static ?Database $instance = null;
+    private PDO $connection;
+
+    private function __construct()
+    {
+        $config = require __DIR__ . '/../Config/database.php';
+
+        try {
+            $this->connection = new PDO(
+                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8",
+                $config['user'],
+                $config['password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
+        } catch (PDOException $e) {
+            die('Database connection error');
+        }
+    }
+
+    public static function getInstance(): Database
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
